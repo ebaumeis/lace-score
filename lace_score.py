ebcbaf1aa2bf9fc5ec1b94bc62dd5ae04d2ba9f5
elizabeth <elizbaumeister@gmail.com>
2018-03-25 17:33:54 -0500

    Join with dx codes and filter to measure

--- lace_score.py
+++ lace_score.py
@@ -6,16 +6,19 @@ def main(spark, measure):
     from extract import get_sample
     from mapping import get_dx_codes, get_comorbidity_cols
 
-    sample_data = get_sample(spark)
-    sample_data.show()
+    # Make sure measure name is uppercase to match mappings
+    measure_name = measure.upper()
 
-    dx_codes = get_dx_codes(spark)
-    dx_codes.show()
+    # Get mappings for diagnosis and comorbidity columns
+    codes = get_dx_codes(spark)
+    comorb_map = get_comorbidity_cols(spark)
 
-    com_cols = get_comorbidity_cols(spark)
-    print com_cols
-    
-    return measure
+    # Get data sample, join to dx codes, and filter to measure
+    data = get_sample(spark)
+    data = data.join(codes, 'diagnosis_code')
+    data = data.filter(data.measure_name == measure_name)
+
+    return data.count()
 
 
 if __name__ == '__main__':
